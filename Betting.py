2754930d8f709d801228bd0b56df881d584b06c4
Aaron <aarsegal@gmail.com>
2023-10-18 20:46:51 -0400

    Half baked betting and AI

--- Betting.py
+++ Betting.py
@@ -1,7 +1,9 @@
-from dataclasses import dataclass
 import pied_poker as pp
+from dataclasses import dataclass
 
 import Deck
+
+WINDOW = 20
 
 ENGLISH_SUITS = {
     Deck.SPADES: 's',
@@ -20,18 +22,164 @@ def hand_to_pp_card(hand):
     return [card_to_pp_card(card) for card in hand]
 
 
-def player_win_chance(hands, community, names, player_idx):
-    players = [pp.Player(names[i], (hand_to_pp_card(hands[player_idx]) if i == player_idx else [])) for i in
-               range(len(names) - 1)]
-    player = players[player_idx]
+def player_win_chance(hand, community, names):
+    players = [pp.Player(names[i], (hand_to_pp_card(hand) if i == 0 else [])) for i in
+               range(len(names))]
+    player = players[0]
 
     community_cards = hand_to_pp_card(community)
     simulator = pp.PokerRoundSimulator(community_cards=community_cards,
                                        players=players,
                                        total_players=len(players))
-    num_simulations = 10000
+    num_simulations = 500
 
     simulation_result = simulator.simulate(n=num_simulations, n_jobs=8, status_bar=False)
 
     probability = simulation_result.probability_of(pp.PlayerWins(player))
     return probability.probability
+
+
+@dataclass
+class PlayerState:
+    cards: list
+    folded: bool
+    raised: bool
+    contribution: int
+    make_decision: callable
+    money: int
+
+    def __init__(self, cards, money, human):
+        self.cards = cards
+        self.money = money
+        if human:
+            self.make_decision = human_bet
+        else:
+            self.make_decision = cpu_bet
+        self.folded = False
+        self.raised = False
+        self.contribution = 0
+
+
+
+@dataclass
+class Round:
+    players: list
+    community: list
+
+    def __init__(self, players):
+        self.players = players
+        self.community = []
+
+    def remaining(self):
+        count = 0
+        for state in self.players:
+            if not state.folded:
+                count += 1
+
+        return count
+
+    def pot_size(self):
+        pot = 0
+        for state in self.players:
+            pot += state.contribution
+
+        return pot
+
+    def reset_between_bets(self):
+        for state in self.players:
+            state.raised = False
+
+    def reset_between_hands(self):
+        for state in self.players:
+            state.folded = False
+            state.raised = False
+            state.cards = []
+            state.contribution = 0
+
+
+def missing_contribution(round, idx):
+    player = round.players[idx]
+    player_contribution = player.contribution
+    for i in range(idx, idx - len(round.players), -1):
+        if not round.players[i].folded and round.players[i].contribution > player_contribution:
+            return round.players[i].contribution - player_contribution
+
+    return 0
+
+
+def cpu_bet(round, idx, names):
+    name = names[idx]
+    still_in_names = [names[i] for i in range(len(round.players)) if not round.players[i].folded]
+    odds = player_win_chance(round.players[idx].cards, round.community, still_in_names)
+    happy_odds = (1 + WINDOW / 100) * odds
+    expected_odds = 1 / len(still_in_names)
+
+    missing = missing_contribution(round, idx)
+
+    budget = round.players[idx].money
+    if missing > budget:
+        round.players[idx].folded = True
+        print(name + " folded.")
+        return
+
+    if missing == 0 and happy_odds < expected_odds:
+        print(name + " checked.")
+        return
+
+    if missing > 0 and happy_odds < expected_odds:
+        round.players[idx].folded = True
+        print(name + " folded.")
+        return
+    if happy_odds >= expected_odds:
+        if odds >= expected_odds:
+            bet = 0 if round.players[idx].raised else int((odds - expected_odds) * budget)
+            bet += missing
+            if bet == missing:
+                print(name + " called.")
+            else:
+                print(name + " raised " + str(bet - missing))
+                round.players[idx].raised = True
+
+            round.players[idx].contribution += bet
+            round.players[idx].money -= bet
+            return
+
+
+        elif missing > 0:
+            round.players[idx].contribution += missing
+            round.players[idx].money -= missing
+            print(name + " called.")
+            return
+        else:
+            print(name + " checked.")
+
+
+def human_bet(round, idx, names):
+    missing = missing_contribution(round, idx)
+
+    pot = round.pot_size()
+    print("Pot: $" + str(pot))
+    print("You have $" + str(round.players[idx].money) + " left")
+    print("$" + str(missing) + " to play...")
+    if missing == 0:
+        choice = input("(C)heck or (r)aise?")
+    else:
+        choice = input("(C)all, (f)old, or (r)aise?")
+
+    if choice.lower() == 'c':
+        round.players[idx].contribution += missing
+        round.players[idx].money -= missing
+
+        print("You " + ("check" if missing == 0 else "call"))
+        return
+    elif choice.lower() == 'f':
+        print("You fold.")
+        round.players[idx].folded = True
+        return
+
+    raise_amount = int(input("How much? "))
+    total_amount = missing + raise_amount
+
+    round.players[idx].contribution += total_amount
+    round.players[idx].money -= total_amount
+    round.players[idx].raised = True
--- Play.py
+++ Play.py
@@ -6,8 +6,16 @@ import Betting
 
 
 def print_odds(hands, community, names, idx):
-    odds = Betting.player_win_chance(hands, community, names[:players], idx) * 100
+    odds = Betting.player_win_chance(hands[idx], community, names[:players]) * 100
     print("Odds of winning: " + format(odds, ".2f") + "%")
+
+
+def play_round(round, names):
+    for i, player in enumerate(round.players):
+        if not player.folded:
+            player.make_decision(round, i, names)
+
+    round.reset_between_bets()
 
 
 names = [
@@ -38,6 +46,9 @@ deck = Deck.new_deck()
 
 hands = Deal.deal_hands(deck, players)
 
+betting_players = [Betting.PlayerState(hands[i], 5000, i == 0) for i in range(players)]
+betting_round = Betting.Round(betting_players)
+
 greeting = "It's just you"
 for i in range(1, players):
     greeting += ', ' + ('and ' if i == players - 1 else '') + names[i]
@@ -48,25 +59,32 @@ print(greeting)
 print("Your hand:")
 Deck.print_hand(hands[0])
 print("You have: " + str(Scoring.score_hand(hands[0], 2)[0][0]))
-print_odds(hands, [], names[:players], 0)
+print_odds(hands, betting_round.community, names[:players], 0)
+play_round(betting_round, names[:players])
 
 input("Do the flop...")
-community = Deal.deal_flop(deck)
-Deck.print_hand(community)
-print("You have: " + str(Scoring.score_hand(hands[0] + community)[0][0]))
-print_odds(hands, community, names[:players], 0)
+betting_round.community = Deal.deal_flop(deck)
+Deck.print_hand(betting_round.community)
+print("You have: " + str(Scoring.score_hand(hands[0] + betting_round.community)[0][0]))
+print_odds(hands, betting_round.community, names[:players], 0)
+play_round(betting_round, names[:players])
+
 
 input("Do the turn...")
-community.append(Deal.deal_single(deck))
-Deck.print_hand(community)
-print("You have: " + str(Scoring.score_hand(hands[0] + community)[0][0]))
-print_odds(hands, community, names[:players], 0)
+betting_round.community.append(Deal.deal_single(deck))
+Deck.print_hand(betting_round.community)
+print("You have: " + str(Scoring.score_hand(hands[0] + betting_round.community)[0][0]))
+print_odds(hands, betting_round.community, names[:players], 0)
+play_round(betting_round, names[:players])
+
 
 input("Do the river...")
-community.append(Deal.deal_single(deck))
-Deck.print_hand(community)
-print("You have: " + str(Scoring.score_hand(hands[0] + community)[0][0]))
-print_odds(hands, community, names[:players], 0)
+betting_round.community.append(Deal.deal_single(deck))
+Deck.print_hand(betting_round.community)
+print("You have: " + str(Scoring.score_hand(hands[0] + betting_round.community)[0][0]))
+print_odds(hands, betting_round.community, names[:players], 0)
+play_round(betting_round, names[:players])
+
 
 input("Do the showdown...")
 
@@ -78,9 +96,9 @@ print("You: ")
 Deck.print_hand(hands[0])
 
 print("Community cards: ")
-Deck.print_hand(community)
+Deck.print_hand(betting_round.community)
 
-winners = Scoring.pick_winner([hand + community for hand in hands])
+winners = Scoring.pick_winner([hand + betting_round.community for hand in hands])
 
 for i, winner in enumerate(winners[0]):
     print(names[winner] + " wins with " + Scoring.describe_winning_hand(winners[1][i]))
